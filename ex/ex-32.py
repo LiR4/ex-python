24556b7b1d33bc2c86df6d56a8a78bda6dce1ffd
Pedro Henrique Lira Da Silva Bispo <83731045+LiR4@users.noreply.github.com>
2021-11-25 17:21:03 -0300

    cpf

--- ex/ex-32.py
+++ ex/ex-32.py
@@ -1,13 +1,37 @@
-listaCPF = []
-multi = 0
-for i in range(11):
-    listaCPF.append(input('informe seu CPF (Só números) '))
+vasco = []
+vascop = []
+cont = 0
+d2 = 0   
+for i in range(11): 
+    vasco.append((input('informe o cpf ')))
 
-for n in range(10, 2):
-    for c in listaCPF:
-        d1 = c * n 
+vascop.append(vasco)
+vasco.pop(10)
+vasco.pop(9)
 
-        if c.isdigit():
-            multi = multi +d1
+for c in vasco:
+    cont = cont + 1
+    C = int(c)
+    d1 =  C * (11  - cont)
+    d2 = d2 + d1
 
-print(multi)
+digi1 = (d2 * 10 ) % 11
+
+vasco.append(digi1) 
+
+cont = 0
+d2 = 0
+d1 = 0
+C = 0
+
+for c in vasco:
+    cont = cont + 1
+    C = int(c)
+    d1 =  C * (12 - cont)
+    d2 = d2 + d1
+
+digi2 = (d2 * 10 ) % 11
+
+vasco.append(digi2)
+
+print(vasco)
---
+++ ex/kkkk.py
@@ -0,0 +1,4 @@
+vasco = [10, 9, 8, 7, 6, 5, 4, 3, 2]
+
+for i in vasco:
+    print(i)
